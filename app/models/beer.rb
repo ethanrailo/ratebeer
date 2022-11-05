754ed3515dab5fe29564184686b2da3895393574
Henri Aalto <henri.aalto@gmail.com>
2022-11-05 15:47:42 +0200

    after 2.4

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,12 @@
 class Beer < ApplicationRecord
-    belongs_to :brewery
-    has_many :ratings
+  belongs_to :brewery
+  has_many :ratings
+
+  def average_rating
+    sum = 0.0
+    self.ratings.each do |rating|
+      sum += rating.score
+    end
+    (sum / self.ratings.count).to_f
+  end
 end
