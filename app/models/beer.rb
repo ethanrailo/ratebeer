83c27ea7f7acf4672236ae87ffd096bdc48f88cd
Henri Aalto <henri.aalto@gmail.com>
2022-11-07 20:23:35 +0200

    after 2.15

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,10 +1,7 @@
 class Beer < ApplicationRecord
+  include RatingAverage
   belongs_to :brewery
   has_many :ratings, dependent: :destroy
-
-  def average_rating
-    self.ratings.inject(0.0) { |sum, rating| sum + rating.score } / self.ratings.size
-  end
 
   def to_s
     "#{self.name}, #{self.brewery.name}"
