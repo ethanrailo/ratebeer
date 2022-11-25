45f82647f3fc79c6752a1c3d3fa6d6b67d1e9922
Henri Aalto <henri.aalto@gmail.com>
2022-11-25 13:54:25 +0200

    after 3.15

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,9 @@ class User < ApplicationRecord
   validates :username, uniqueness: true, length: { minimum: 3, maximum: 30 }
   validates :password, format: { with: /\A(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{4,}\z/,
                                  message: "min length 4 chars. include at least one lowercase character, one uppercase character and one number" }
-  has_many :ratings   # käyttäjällä on monta ratingia
+  has_many :ratings, dependent: :destroy
   has_many :beers, through: :ratings
-  has_many :memberships
+  has_many :memberships, dependent: :destroy
   has_many :beer_clubs, through: :memberships
   has_secure_password
 
