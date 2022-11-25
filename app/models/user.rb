82a3fcebe9cac8837c86f80e66137f45d2e53b9a
Henri Aalto <henri.aalto@gmail.com>
2022-11-25 13:06:40 +0200

    after 3.12

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 class User < ApplicationRecord
   include RatingAverage
   validates :username, uniqueness: true, length: { minimum: 3, maximum: 30 }
+  validates :password, format: { with: /\A(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{4,}\z/,
+                                 message: "min length 4 chars. include at least one lowercase character, one uppercase character and one number" }
   has_many :ratings   # käyttäjällä on monta ratingia
   has_many :beers, through: :ratings
   has_many :memberships
   has_many :beer_clubs, through: :memberships
+  has_secure_password
 
   def to_s
     username
